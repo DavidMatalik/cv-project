0454e4c6648e8365d0125fed7e6aa38c2baf1f65
David Matalik <david.matalik@posteo.de>
2021-05-03 10:04:48 +0200

    Beautify App.js through consistency

--- src/App.js
+++ src/App.js
@@ -3,20 +3,16 @@ import CVSection from './components/CVSection'
 function App() {
   const generalFields = ['Name', 'Email', 'Phone']
   const educationFields = ['School name', 'Title of Study', 'Date of Study']
-  const practiceFields = [
-    'Company Name',
-    'Position title',
-    'Main tasks',
-    'Start Date',
-    'End Date',
-  ]
+  // prettier-ignore
+  const practiceFields = ['Company Name', 'Position title', 'Main tasks', 'Start Date', 'End Date']
+
   return (
     <div className='App'>
       <h1>Create your CV!</h1>
-      <CVSection
-        title='General Information about You'
-        fields={generalFields}
-      ></CVSection>
+      {/* prettier-ignore*/}
+      <CVSection 
+        title='General Information' 
+        fields={generalFields}></CVSection>
       <CVSection
         title='Educational experience'
         fields={educationFields}
