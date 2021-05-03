35becce97c4e161ea842a5d70b2cb29912e7220c
David Matalik <david.matalik@posteo.de>
2021-05-03 10:05:24 +0200

    Control input value with state

--- src/components/CVSectionInput.js
+++ src/components/CVSectionInput.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 
 class CVSectionInput extends Component {
+  constructor() {
+    super()
+    this.state = {
+      inputValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(ev) {
+    this.setState({
+      inputValue: ev.target.value,
+    })
+  }
+
   render() {
+    const { inputValue } = this.state
+    const { inputName } = this.props
     return (
       <div>
-        <label>{this.props.inputName}</label>
-        <input type='text' />
+        <label>{inputName}</label>
+        <input type='text' value={inputValue} onChange={this.handleChange} />
       </div>
     )
   }
