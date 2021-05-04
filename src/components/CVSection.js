87a4780f1e44a695d2fbbf631bcabea347ecf0fd
David Matalik <david.matalik@posteo.de>
2021-05-04 09:39:22 +0200

    Integrate input fields into a form
    This needed to be done because for later development steps
    the value of input fields is needed in CVSectionForm.
    Another reason for this change is to control the values of
    input fields with React.

--- src/components/CVSection.js
+++ src/components/CVSection.js
@@ -1,32 +1,19 @@
 import React, { Component } from 'react'
 import CVSectionHeading from './CVSectionHeading'
-import CVSectionInput from './CVSectionInput'
-import CVSectionEditButton from './CVSectionEditButton'
-import CVSectionSubmitButton from './CVSectionSubmitButton'
+import CVSectionForm from './CVSectionForm'
 
 class CVSection extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
   }
 
   render() {
     const { title, fields } = this.props
-    const inputs = fields.map((field, i) => {
-      return (
-        <div className='input-wrapper' key={i}>
-          <CVSectionInput inputName={field}></CVSectionInput>
-        </div>
-      )
-    })
 
     return (
       <section className='cv-section'>
         <CVSectionHeading title={title}></CVSectionHeading>
-        {inputs}
-        <div id='buttons'>
-          <CVSectionEditButton></CVSectionEditButton>
-          <CVSectionSubmitButton></CVSectionSubmitButton>
-        </div>
+        <CVSectionForm fields={fields}></CVSectionForm>
       </section>
     )
   }
---
+++ src/components/CVSectionForm.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react'
+import CVSectionSubmitButton from './CVSectionSubmitButton'
+import CVSectionEditButton from './CVSectionEditButton'
+
+class CVSectionForm extends Component {
+  constructor(props) {
+    super(props)
+
+    const { fields } = this.props
+
+    // Create array to generate unique names for existing input fields
+    this.fieldObjects = fields.map((field, i) => {
+      return {
+        labelName: field,
+        inputName: `inputField${i}`,
+      }
+    })
+
+    this.state = {
+      inputNames: [],
+    }
+
+    // Put existing input names into state inputNames array
+    this.fieldObjects.forEach((obj) => {
+      this.setState({
+        inputNames: this.state.inputNames.push({ [obj.inputName]: '' }),
+      })
+    })
+
+    console.log(this.state)
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  // This handleChange works for all input Fields
+  // in this form. It puts the changed value to
+  // the correspending input field using the name
+  // attribute of the changed input field
+  handleChange(ev) {
+    this.setState({
+      [ev.target.name]: ev.target.value,
+    })
+  }
+
+  render() {
+    const { inputValue } = this.state
+    const inputs = this.fieldObjects.map((obj, i) => {
+      return (
+        <div className='input-wrapper' key={i}>
+          <label>{obj.labelName}</label>
+          <input
+            type='text'
+            name={obj.inputName}
+            value={inputValue}
+            onChange={this.handleChange}
+          />
+        </div>
+      )
+    })
+
+    return (
+      <form>
+        {inputs}
+        <div id='buttons'>
+          <CVSectionEditButton></CVSectionEditButton>
+          <CVSectionSubmitButton></CVSectionSubmitButton>
+        </div>
+      </form>
+    )
+  }
+}
+
+export default CVSectionForm
